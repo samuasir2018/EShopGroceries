530fb52639be733476bbd6bced4755af4343588d
samuasir2018 <samuasir2018@gmail.com>
2022-04-19 22:19:08 +0200

    Nuevo directorio para vistas

--- view/buylist.php
+++ view/buylist.php
@@ -1,9 +1,9 @@
 <?php
-    require_once('php/daoProduct.php');
-    require_once('php/daoBasket.php');
-    require_once('php/basket.php');
-    require_once('php/daoUser.php');
-    require_once('php/daoBuylist.php');
+    require_once('../php/daoProduct.php');
+    require_once('../php/daoBasket.php');
+    require_once('../php/basket.php');
+    require_once('../php/daoUser.php');
+    require_once('../php/daoBuylist.php');
 ?>
 
 <!doctype html>
@@ -17,11 +17,11 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
         <!--Css propio-->
-        <link rel="stylesheet" type="text/css" href="css/style_main.css"></link>
-        <link rel="stylesheet" type="text/css" href="css/style_store.css"></link>
+        <link rel="stylesheet" type="text/css" href="../css/style_main.css"></link>
+        <link rel="stylesheet" type="text/css" href="../css/style_store.css"></link>
 
         <title>EShopGroceries - Buylist</title>
-        <link rel="icon" type="image/x-icon" href="img/logo/logo.png">
+        <link rel="icon" type="image/x-icon" href="../img/logo/logo.png">
     </head>
 
     <?php
@@ -32,7 +32,7 @@
 
         <div class="row">
             <div class="col-9 padding-Logo ">
-                <img class="logo" src="img/logo/logo.png"></img>
+                <img class="logo" src="../img/logo/logo.png"></img>
             </div>
             <div class="col-1 padding-0 padding-Menu">
                 <div class="menuLateralDerecho">
@@ -111,7 +111,7 @@
                             <h3>Carrito</h3>
                         </div>
                         <div class="col">
-                            <img class="carritoIMG" src="./img/icons/Basket.PNG">
+                            <img class="carritoIMG" src="../img/icons/Basket.PNG">
                         </div>
                     </div>
                     <hr></hr>
@@ -131,8 +131,8 @@
         <!-- Option 1: Bootstrap Bundle with Popper -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
         <!-- JS propio -->
-        <script src="js/jquery-3.2.1.min.js"></script>
-        <script src="js/basketController.js"></script>
+        <script src="../js/jquery-3.2.1.min.js"></script>
+        <script src="../js/basketController.js"></script>
     </body>
     <footer id="footerZone">
         <hr>
@@ -142,10 +142,7 @@
                     <br> Telf: 555-5555-555-55 -  e-mail: EShopGroceries@Groceries.com<br><br>
                 <i>Todos los derechos reservados.</i><br><br>
                 Web desarrollada por el alumno de Desarrollo de aplicaciones web en el I.E.S Juan Bosco,
-                <i>Samuel</i><br><br>
-
-                Enlaces de Interés:</b>
-
+                <i>Samuel</i>
                 </p>
             </div>
             <div class="col-8 rightColumnFooter">
--- view/index.php
+++ view/index.php
@@ -9,17 +9,17 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
         <!--Css propio-->
-        <link rel="stylesheet" type="text/css" href="css/style_main.css"></link>
+        <link rel="stylesheet" type="text/css" href="../css/style_main.css"></link>
 
         <title>EShopGroceries - Inicio de sesión</title>
-        <link rel="icon" type="image/x-icon" href="img/logo/logo.png">
+        <link rel="icon" type="image/x-icon" href="../img/logo/logo.png">
     </head>
 
     <body>
 
         <div class="row">
             <div class="col-9 padding-Logo ">
-                <img class="logo" src="img/logo/logo.png"></img>
+                <img class="logo" src="../img/logo/logo.png"></img>
             </div>
             <div class="col-1 padding-0 padding-Menu">
                 <div class="menuLateralDerecho">
@@ -95,7 +95,7 @@
 
     <?php
         session_start(); //Comienzo la sesion
-        require_once('php/daoUser.php');
+        require_once('../php/daoUser.php');
         if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['enviarRegistro'])){//Compruebo si se ha pulsado el boton del login
             logicaLogin();//Llamo a la funcion logicaLogin
         }
@@ -120,10 +120,7 @@
                     <br> Telf: 555-5555-555-55 -  e-mail: EShopGroceries@Groceries.com<br><br>
                 <i>Todos los derechos reservados.</i><br><br>
                 Web desarrollada por el alumno de Desarrollo de aplicaciones web en el I.E.S Juan Bosco,
-                <i>Samuel</i><br><br>
-
-                Enlaces de Interés:</b>
-
+                <i>Samuel</i>
                 </p>
             </div>
             <div class="col-8 rightColumnFooter">
