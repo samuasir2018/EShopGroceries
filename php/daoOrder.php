9eed9c5b51732e2eb3fe0c7ce367ec0d2afeae1a
samuasir2018 <samuasir2018@gmail.com>
2022-04-20 01:37:17 +0200

    nuevo nombre base de datos

--- php/daoOrder.php
+++ php/daoOrder.php
@@ -10,7 +10,7 @@
 
         public function __construct()
         {
-            $this->objDB = new DB ('eshopgroceries');
+            $this->objDB = new DB ('id18809134_eshopgroceries');
         }
 
         public function printOrderHistory($email){
